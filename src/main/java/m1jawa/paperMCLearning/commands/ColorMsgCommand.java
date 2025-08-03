b53426cf22d16de68e6c01bb261b01afe11b787f
m1jawa <nameersan@gmail.com>
2025-08-03 17:33:35 +0200

    added /color-msg command

--- src/main/java/m1jawa/paperMCLearning/commands/ColorMsgCommand.java
+++ src/main/java/m1jawa/paperMCLearning/commands/ColorMsgCommand.java
@@ -1,5 +1,7 @@
 package m1jawa.paperMCLearning.commands;
 
+import com.mojang.brigadier.Message;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,23 +16,35 @@ import java.util.List;
 public class ColorMsgCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
-        if (strings.length < 1) return false;
         Player player = (Player) commandSender;
+        if (strings.length < 2) {
+            player.sendMessage("§cYou didn't write all arguments!");
+            return false;
+        }
+
+        Player target = Bukkit.getPlayer(strings[0]);
+
+        if (target == null) {
+            player.sendMessage("§cPlayer is not online!");
+            return false;
+        }
+
         String message = "";
 
-        if (strings[0].equals("red")) {
+        if (strings[1].equals("red")) {
             message = "§4";
-        } else if (strings[0].equals("blue")) {
+        } else if (strings[1].equals("blue")) {
             message = "§1";
-        } else if (strings[0].equals("green")) {
+        } else if (strings[1].equals("green")) {
             message = "§2";
         }
 
-        for ( int i = 1; i < strings.length; i ++) {
+        for ( int i = 2; i < strings.length; i ++) {
             message += strings[i] + " ";
         }
 
-        player.sendMessage(message);
+        player.sendMessage("You wrote to " + target.getName() + ": " + message);
+        target.sendMessage(player.getName() + " wrote you: " + message);
 
         return false;
     }
@@ -40,6 +54,12 @@ public class ColorMsgCommand implements CommandExecutor, TabCompleter {
         List<String> tab = new ArrayList<>();
 
         if (strings.length == 1) {
+            for (Player players : Bukkit.getOnlinePlayers()) {
+                tab.add(players.getName());
+            }
+        }
+
+        if (strings.length == 2) {
             tab.add("red");
             tab.add("blue");
             tab.add("green");
