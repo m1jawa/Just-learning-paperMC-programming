3e935181f54a94c2d422b143aa75c6f25baf6620
m1jawa <nameersan@gmail.com>
2025-08-03 11:14:04 +0200

    Added /roll command

--- src/main/java/m1jawa/paperMCLearning/PaperMCLearning.java
+++ src/main/java/m1jawa/paperMCLearning/PaperMCLearning.java
@@ -1,17 +1,12 @@
 package m1jawa.paperMCLearning;
 
+import m1jawa.paperMCLearning.commands.RollCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PaperMCLearning extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getCommand("roll").setExecutor(new RollCommand());
     }
 }
--- src/main/java/m1jawa/paperMCLearning/commands/RollCommand.java
+++ src/main/java/m1jawa/paperMCLearning/commands/RollCommand.java
@@ -1,4 +1,22 @@
 package m1jawa.paperMCLearning.commands;
 
-public class RollCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class RollCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (!(commandSender instanceof Player)) return false;
+        Player player = (Player) commandSender;
+        Random random = new Random();
+        int num = random.nextInt(1, 21);
+        player.sendMessage("You got " + num);
+        return false;
+    }
 }
