d28532ceefbf34186459aea65659e8758666261e
m1jawa <nameersan@gmail.com>
2025-08-03 11:03:51 +0200

    initial commit

---
+++ src/main/java/m1jawa/paperMCLearning/PaperMCLearning.java
@@ -0,0 +1,17 @@
+package m1jawa.paperMCLearning;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperMCLearning extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
