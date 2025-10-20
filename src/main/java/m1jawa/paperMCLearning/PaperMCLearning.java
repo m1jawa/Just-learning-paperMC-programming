4fe7ac86fbc5f1ff8ea1df5672b2089b37fc979a
m1jawa <nameersan@gmail.com>
2025-10-20 21:16:56 +0200

    Added some particles to later command

--- src/main/java/m1jawa/paperMCLearning/PaperMCLearning.java
+++ src/main/java/m1jawa/paperMCLearning/PaperMCLearning.java
@@ -7,6 +7,7 @@ import m1jawa.paperMCLearning.Listeners.JoinQuitListener;
 import m1jawa.paperMCLearning.Listeners.PlayerRightClickListener;
 import m1jawa.paperMCLearning.commands.ColorMsgCommand;
 import m1jawa.paperMCLearning.commands.GetItemCommand;
+import m1jawa.paperMCLearning.commands.LaterCommand;
 import m1jawa.paperMCLearning.commands.RollCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
@@ -14,21 +15,27 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.print.Paper;
+
 
 public final class PaperMCLearning extends JavaPlugin {
 
-
+    static PaperMCLearning plugin;
 
     @Override
     public void onEnable() {
 
+        plugin = this;
+
         BasicCommand getItemCommand = new GetItemCommand();
         BasicCommand colorMsgCommand = new ColorMsgCommand();
         BasicCommand rollCommand = new RollCommand();
+        BasicCommand laterCommand = new LaterCommand();
 
         registerCommand("get-item", getItemCommand);
         registerCommand("color-msg", colorMsgCommand);
         registerCommand("roll", rollCommand);
+        registerCommand("later", laterCommand);
 
 
         PluginManager pluginManager = Bukkit.getPluginManager();
@@ -39,5 +46,9 @@ public final class PaperMCLearning extends JavaPlugin {
                 (Plugin) this);
 
         PluginItems.init();
+    }
+
+    public static PaperMCLearning getPlugin(){
+        return plugin;
     }
 }
--- src/main/java/m1jawa/paperMCLearning/commands/LaterCommand.java
+++ src/main/java/m1jawa/paperMCLearning/commands/LaterCommand.java
@@ -1,4 +1,93 @@
 package m1jawa.paperMCLearning.commands;
 
-public class LaterCommand {
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import m1jawa.paperMCLearning.PaperMCLearning;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LaterCommand implements BasicCommand {
+
+    @Override
+    public void execute(CommandSourceStack source, String[] args) {
+        CommandSender sender = source.getSender();
+        if (sender instanceof Player player) {
+            World world = player.getWorld();
+
+            if (args[0].equals("test")) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        player.sendMessage("test");
+                    }
+                }.runTaskLater(PaperMCLearning.getPlugin(), 60);
+            } else if (args[0].equals("cycle")) {
+                new BukkitRunnable() {
+
+                    Random random = new Random();
+                    int timer = 0;
+
+                    @Override
+                    public void run() {
+
+                        timer++;
+                        player.sendMessage("cycle (every 1s)");
+
+                        Vector vector = new Vector(random.nextFloat(-7, 7), random.nextFloat(1, 7), random.nextFloat(-7, 7));
+                        player.setVelocity(vector);
+
+                        if(timer == 10) {
+                            player.sendMessage("Stop");
+                            cancel();
+                        }
+
+                    }
+                }.runTaskTimer(PaperMCLearning.getPlugin(), 0, 10);
+            } else if (args[0].equals("particle")) {
+
+                world.spawnParticle(Particle.FLAME, player.getLocation(), 10, 0, 0.5, 2, 0.1);
+
+            } else if (args[0].equals("particle-line")) {
+
+                Location location = player.getEyeLocation();
+
+                for (int i = 0; i < 50; i++) {
+                    location.add(location.getDirection());
+                    world.spawnParticle(Particle.END_ROD, location, 1, 0, 0, 0, 0);
+
+                    if (location.getBlock().isSolid()) {
+                        i = 50;
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public @Nullable List<String> suggest(CommandSourceStack source, String[] args){
+        List<String> tab = new ArrayList<>();
+
+        if (args.length == 1) {
+            tab.add("test");
+            tab.add("cycle");
+            tab.add("particle");
+            tab.add("particle-line");
+        }
+
+        return tab;
+    }
+
 }
